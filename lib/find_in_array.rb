a2ab117c01e22990bba1cfe1b6eae7d7106394fa
Scot Sexton <rss3509@gmail.com>
2020-05-14 00:50:30 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,8 @@
 def find_element_index(array, value_to_find)
   i = 0 
   length = array.length()
-  while i< length do
-    puts array[i]==value_to_find
+  while i < length do
+    puts array[i] == value_to_find
     i += 1 
   end
-    
 end
